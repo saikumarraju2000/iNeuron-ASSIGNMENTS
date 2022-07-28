fe0556090409b21dcf5feb7ccbf6801e63f607ff
Sai Kumar Raju <105580811+saikumarraju2000@users.noreply.github.com>
2022-07-28 11:37:03 +0530

    Add files via upload
    These are the 5 problems in Assignment-1 about pattern programming logic.

---
+++ problem2.java
@@ -0,0 +1,21 @@
+package assignment1;
+
+public class problem2 
+{
+	public static void main(String[] args)
+	{
+		
+		int count=1;
+		for(int i=1; i<=4;i++)
+		{
+			System.out.println(i+" "+i+" "+i+" "+i);
+			
+			
+		}
+		
+	}
+	
+	
+	
+	
+}
